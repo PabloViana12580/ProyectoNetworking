fb8342e115aeaf114b5aed8de27f2cbacf3ca8fb
→∟Paola Fuentes Caro← <Paola Fuentes Caro>
2017-04-06 20:09:47 -0600

    GUI inicial :instagram, tinderUVG sin funcionalidad. QR reader funcionando

---
+++ tinderUVG/Proyecto/app/src/main/java/com/example/analuciadiazleppe/proyecto/proyecto.java
@@ -0,0 +1,13 @@
+package com.example.analuciadiazleppe.proyecto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class proyecto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proyecto);
+    }
+}
