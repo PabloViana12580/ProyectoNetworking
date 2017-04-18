32908ab710084afaf3fe2259c733ba6c5844fc03
Pablo Díaz <pablo5_diaz@hotmail.com>
2017-04-18 10:23:32 -0600

    add login with Facebook

---
+++ Login/app/src/main/java/com/example/android/facebookloginsample/PrefUtils.java
@@ -0,0 +1,29 @@
+package com.example.android.facebookloginsample;
+
+import android.content.Context;
+
+/**
+ * Class to handle current user logged
+ */
+public class PrefUtils {
+
+    public static void setCurrentUser(User currentUser, Context ctx){
+        ComplexPreferences complexPreferences = ComplexPreferences.getComplexPreferences(ctx, "user_prefs", 0);
+        complexPreferences.putObject("current_user_value", currentUser);
+        complexPreferences.commit();
+    }
+
+    public static User getCurrentUser(Context ctx){
+        ComplexPreferences complexPreferences = ComplexPreferences.getComplexPreferences(ctx, "user_prefs", 0);
+        User currentUser = complexPreferences.getObject("current_user_value", User.class);
+        return currentUser;
+    }
+
+    public static void clearCurrentUser( Context ctx){
+        ComplexPreferences complexPreferences = ComplexPreferences.getComplexPreferences(ctx, "user_prefs", 0);
+        complexPreferences.clearObject();
+        complexPreferences.commit();
+    }
+
+
+}
---
+++ Login/app/src/main/java/com/example/android/facebookloginsample/User.java
@@ -0,0 +1,29 @@
+package com.example.android.facebookloginsample;
+
+/**
+ * User Model.
+ * Public for easy access, should not be used to modify object
+ */
+public class User {
+
+
+    public String name;
+
+    public String email;
+
+    public String facebookID;
+
+    public String gender;
+
+    public User(String name, String email, String facebookID, String gender) {
+        this.name = name;
+        this.email = email;
+        this.facebookID = facebookID;
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "User: " + this.name + " ID " + facebookID;
+    }
+}
